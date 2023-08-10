4bc722bf9d406368462699eec94e6ddb22870c24
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-10 08:45:14 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
-	int puts(str);
+	char str[] = """Programming is like building a multilingual puzzle";
+	puts(str);
 	return (0);
 }
