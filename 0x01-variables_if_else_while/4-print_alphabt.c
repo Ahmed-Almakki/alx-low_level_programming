7bd8b9171bfd16c3d8e1a9f71395a19ba2911814
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-11 10:00:02 -0700

    if lwr

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int ln = 0;
+	char lwr;
+
+	ln = strlen(str);
+	while (i < ln)
+	{
+		lwr = tolower(str[i]);
+		if ((lwr == 'q') | (lwr == 'e'))
+		{
+			i++;
+		}
+		else
+		{
+			putchar(lwr);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
