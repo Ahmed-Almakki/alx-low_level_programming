6e2b1f7630fd222a0f45084fba249458cf6ecd64
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-11 10:42:08 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	char ch;
+	int ln;
+
+	ln = strlen(str);
+	while (ln >= 0)
+	{
+		ch = tolower(str[ln]);
+		putchar(ch);
+		ln--;
+	}
+	putchar('\n');
+	return (0);
+}
