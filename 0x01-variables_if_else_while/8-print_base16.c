c829d5276165cf776f0cc88f1bae0f10854805c2
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-11 16:53:43 -0700

    Hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Enrty point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	char l;
+
+	for (d = '0'; d<= '9'; d++)
+	{
+		putchar(d);
+	}
+	for (l = 'a'; low <= 'f'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
