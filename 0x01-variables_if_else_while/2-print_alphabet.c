2aae81c35544c1b803e2a4ae0fe50599a5af85a8
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-11 09:01:58 -0700

    lower c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,9 @@ int main(void)
 {
 	char str[] = "AbcdEfgHijKlmNopQrsTuvWxyZ";
 	int i = 0;
+	int k = 0;
 
-	while (str[i] != '\0')
+	while (k <= strlen(str))
 	{
 		str[i] = tolower(str[i]);
 		putchar(str[i]);
