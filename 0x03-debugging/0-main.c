43287f7046d5695d6caeab3f94a08ac8b70ec778
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-17 12:57:28 -0700

    0-task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - ests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or negative(i);
+	return (0);
+}
