70290cf38d229f790affbeee183cdd85cd2c9323
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-17 14:57:26 -0700

    0-task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+
+/**
+ * function test if is upper
+ * Return : 0 if is upper
+ * Return : 1 else
+ */
+
+int _isupper(char c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
