8fe934f1539364181f2f70cbdcadaa15e3af575f
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-18 12:54:52 -0700

    tasks

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_line : function
+ * print_line - function
  * @n: line length
  * Return: 0
  */
 void print_line(int n)
 {
 	int c = 0;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
