05a9a5249e143bc640fc8a065e00ecff94bc1650
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-21 15:52:16 -0700

    stirng

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - function
+ * @s : pointer
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	for (i = l - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+}
