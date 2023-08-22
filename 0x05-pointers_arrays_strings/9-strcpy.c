209aca86684e92bc89b3a2e0bbce338debb6aae3
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-21 17:16:43 -0700

    cpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - function
+ * @dest: operand
+ * @src: second opernad
+ * Return: characters
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return 	(strcpy(dest, src));
+}
