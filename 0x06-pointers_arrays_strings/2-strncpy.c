0fceda30a13f792ec2a3e81c521bf0aac6f92798
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-24 07:28:51 -0700

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,23 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	if (dest == NULL || src == NULL || n == 0)
+	{
+		return (dest);
+	}
+
+	while ((i < n) && (src[i] != '\0'))
 	{
 		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
