b9012f8db9e8bdf14e47e48b96f8835e98b2c931
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-08-29 11:35:49 -0700

    a

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - fun
+ * @dest: ip
+ * @src: op
+ * @n: op
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
