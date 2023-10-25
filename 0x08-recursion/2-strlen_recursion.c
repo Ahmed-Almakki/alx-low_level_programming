5af6a6fb8ed49e7d93d68040b2d7c70ca6c4ff27
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-10-25 16:48:05 -0700

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,15 +9,12 @@
 
 int length(int i, char *s)
 {
-	if (*s !='\0')
-	{
-		i = i + 1;
-		length(i, (s + 1));
-	}
-	if (*s == '\0')
+	if (*s =='\0')
 	{
 		return (i);
 	}
+	i = i + 1;
+	return (length(i, (s + 1)));
 }
 
 /**
@@ -31,7 +28,6 @@ int _strlen_recursion(char *s)
 	int j;
 
 	i = 0;
-	j = 0;
 	j = length(i, s);
 	return (j);
 }
