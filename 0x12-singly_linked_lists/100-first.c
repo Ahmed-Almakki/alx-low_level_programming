202a186c7b57ceffe02be4f50eb4e7e1b1b04612
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-11-17 16:31:04 -0800

    befor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * befr - fun
+ */
+void __attribute__((constructor)) befr(void);
+
+void befr(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
