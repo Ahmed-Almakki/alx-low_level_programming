7b217c7e87a822a59ee9e8a5c53f0a0fc883be99
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-11-17 16:06:07 -0800

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - fun
+ * @head: param
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
