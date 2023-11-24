e99e1fd89555f736ead37ad1544a0ee0c5ddd7a6
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-11-24 04:16:23 -0800

    f

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - fun
+ * Return: int
+ */
+int get_endianness(void)
+{
+	int  i = 1;
+	char *p = (char *)&i;
+
+	if (*p+'0' == '1')
+		return (1);
+	return (0);
+}
