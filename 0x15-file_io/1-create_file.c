b9ae6fa6e6073d4dd02ba749ebac05d7f805bf49
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2023-11-27 14:12:31 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,17 +9,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len;
+	int fd, wr;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_contnt == NULL)
+	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IWUSR | S_IRUSR);
 		if (fd == -1)
 			return (-1);
 		return (1);
 	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IWUSR | S_IRUSR);
+	if (fd == -1)
+		return (-1);
 	wr = write(fd, text_content, strlen(text_content));
 	if (wr == -1)
 		return (-1);
