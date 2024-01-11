0337f43b971ac834d01d94f8e6a5ea4bb3c6d2d7
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-01-11 05:39:22 -0800

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_dlistint - func
+ * @h: pointer
+ * Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h):
+{
+	size_t i = 0;
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
