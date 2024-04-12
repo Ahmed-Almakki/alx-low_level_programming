ee864b285335cb2bf80a699ba97e988256609ced
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-04-12 16:33:22 -0700

    1st commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	tmp->size = size;
-	tmp->array = malloc((sizeof(hash_node_t *)) * size);	
+	tmp->array = malloc((sizeof(hash_node_t *)) * size);
 	for (i = 0; i < size; i++)
 	{
 		tmp->array[i] = NULL;
