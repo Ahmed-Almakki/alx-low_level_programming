464906b55aae157f495ad38d5e046119bcc6f395
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-15 17:40:34 -0700

    order of if condition

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,19 +14,18 @@ int binary_search(int *array, size_t size, int value)
 	int i = 0;
 	size_t last = size - 1;
 	size_t beg = 0;
+	size_t j;
 
 	while (true)
 	{
-		i = (beg + last) / 2;
-		if (array[i] == value)
+		printf("Searching in array:");
+		for (j = beg; j < last; j++)
 		{
-			i = (int) i;
-			break;
+			printf("%d, ", array[j]);
 		}
-		if (value < array[i])
-			last = (size_t)i - 1;
-		if (value > array[i])
-			beg = (size_t)i + 1;
+		printf("%d\n", array[last]);
+
+		i = (beg + last) / 2;
 		if (beg == last)
 		{
 			if ((array[i] != value) || (array == NULL))
@@ -34,7 +33,15 @@ int binary_search(int *array, size_t size, int value)
 				i = -1;
 				break;
 			}
+		}		if (array[i] == value)
+		{
+			i = (int) i;
+			break;
 		}
+		if (value < array[i])
+			last = (size_t)i - 1;
+		if (value > array[i])
+			beg = (size_t)i + 1;	
 	}
 	return (i);
 }
